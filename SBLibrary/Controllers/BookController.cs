396a888b54ab9e0ef16b58f18f3d9ced2dd930e8
Sathya-Projects <58175457+Sathya-Projects@users.noreply.github.com>
2021-07-12 12:21:24 +0100

    File Upload logic implemented. In-progress

--- SBLibrary/Controllers/BookController.cs
+++ SBLibrary/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using SBLibrary.Service.IService;
 using SBLibrary.Service.Service;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -176,6 +177,36 @@ namespace SBLibrary.Controllers
             }
             catch
             {
+                return View();
+            }
+
+        }
+
+        // GET: Upload Book
+        [HttpGet]
+        public ActionResult UploadBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadBook(HttpPostedFileBase file)
+        {
+            try
+            {
+
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
                 return View();
             }
         }
